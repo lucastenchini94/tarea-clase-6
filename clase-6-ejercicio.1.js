43f8b995e993b597922613342fd20c50260529f4
lucastenchini94 <lucastenchini94@gmail.com>
2024-11-28 15:50:51 -0300

    cartel-de-Errores
    agregue un cartel de "arreglar errores de escritura" para que vea los errores y no se agreguen los input hasta corregir y que tampoco se hagan las operacioens

--- clase-6-ejercicio.1.js
+++ clase-6-ejercicio.1.js
@@ -3,6 +3,7 @@ const botonCalcularParametros = document.querySelector("#calculo-de-parametros")
 const textCalculos = document.querySelector("h2")
 const botonRestablecer = document.querySelector("#restablecer-todo")
 const nodoPaginaform = document.querySelector("form")
+
 botonRestablecer.onclick = function () {
     location.reload()
     return false
@@ -11,7 +12,10 @@ botonRestablecer.onclick = function () {
 botonIngresarInput.onclick = function () {
     borrarIntegrantes();
     const cantPersonas = document.querySelector("#cantidad-personas").value
-    let contUno = 0
+    
+    console.log(validarCantidadDePersonas(cantPersonas)); 
+    if (validarCantidadDePersonas(cantPersonas)=== "") {
+        let contUno = 0
     while (contUno<cantPersonas) {
         
         const nuevoLi = document.createElement("li")
@@ -23,7 +27,11 @@ botonIngresarInput.onclick = function () {
         contUno++
         
     }
-    console.log(validarCantidadDePersonas(cantPersonas));    
+    }else{
+        alert("fijarse errores de escritura");
+    }
+    
+       
 
     return false
 }
@@ -37,14 +45,25 @@ function borrarIntegrantes() {
 botonCalcularParametros.onclick = function(){
     const edadesF = document.querySelectorAll(".edades")
     const edades = []
+    let contadorErorres = 0;
     for (let i = 0; i < edadesF.length; i++) {
-        edades.push(Number(edadesF[i].value))   
+        edades.push(Number(edadesF[i].value))
+        console.log(validarEdades(edades[i]));
+        
+        if (validarEdades(edades[i])==="") {
+            
+        }else{
+            contadorErorres++;
+        }
     }
-    let menorEdad = 1000
-    let mayorEdad = 0
+
+    if (contadorErorres === 0) {
+        let menorEdad = 1000
+        let mayorEdad = 0
     
-    let sumadorEdades = 0
-    for (let i = 0; i < edades.length; i++) {
+        let sumadorEdades = 0
+      for (let i = 0; i < edades.length; i++) {
+      
         if (edades[i]<menorEdad) {
             menorEdad = edades[i]
         }   
@@ -52,11 +71,16 @@ botonCalcularParametros.onclick = function(){
             mayorEdad=edades[i]
         }
         sumadorEdades=sumadorEdades+edades[i]
-        console.log(validarEdades(edades[i]));
-    }
-    const promedio = sumadorEdades/edades.length
+        
+      }
+       const promedio = sumadorEdades/edades.length
 
-    textCalculos.innerText = ("el familiar con menor edad tiene "+menorEdad+" años, el mayor tiene "+mayorEdad+" años y el promedio de edad es de "+parseInt(promedio))
+       textCalculos.innerText = ("el familiar con menor edad tiene "+menorEdad+" años, el mayor tiene "+mayorEdad+" años y el promedio de edad es de "+parseInt(promedio))
+
+    }else{
+        alert("corregir errores de escritura")
+    }
+    
     
 
 
@@ -67,24 +91,31 @@ botonCalcularParametros.onclick = function(){
 }
 
 function validarCantidadDePersonas(cantPersonas) {
+     
     if (cantPersonas === "0") {
         return "este campo debe tener 1 caracter o mas";
+        
     }
     if (cantPersonas === "") {
         return "este campo debe tener 1 caracter o mas";
+        
     }
     if (cantPersonas % 1 !== 0) {
         return "el campo solo admite numeros enteros";
+        
     }else{
     return "";
     }
 }
 function validarEdades(edades) {
+    
     if (edades === 0) {
-        return "este campo no puede estar vaco, por favor ingrese una edad";
+        return "este campo no puede estar vacio, por favor ingrese una edad";
+        
     }
     if (edades % 1 !== 0) {
         return "el campo solo admite numeros enteros";
+        
     }else{
         return "";
     }
